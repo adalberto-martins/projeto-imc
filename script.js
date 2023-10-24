db98fc2d560d85530866dc5ee2407bb25bc5af5d
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2023-10-23 21:34:30 -0300

    update

--- script.js
+++ script.js
@@ -24,30 +24,34 @@ function verificar() {
             genero = 'Mulher'
         }
 
-        if (imc < 18.5) {
+        if (imc <= 18.5) {
             mensagemIMC = 'Abaixo do Peso'
             img.setAttribute('src', genero === 'Homem' ? 'imagens/magro_homem.png' : 'imagens/magro_mulher.png')
             alert('Abaixo do Peso')
-        } else if (imc < 24.9) {
+        } else if (imc >= 18.6 && imc <= 24.9) {
             mensagemIMC = 'Peso ideal (Parabéns!)'
             img.setAttribute('src', genero === 'Homem' ? 'imagens/normal_homem.png' : 'imagens/normal_mulher.png')
             alert('Peso ideal (Parabéns!)')
-        } else if (imc < 29.9) {
-            mensagemIMC = 'Levemente acima do Peso';
+        } else if (imc >= 25 && imc <=29.9) {
+            mensagemIMC = 'Levemente acima do Peso'
             img.setAttribute('src', genero === 'Homem' ? 'imagens/sobrepeso_masculino.png' : 'imagens/sobrepeso_mulher.png')
             alert('Levemente acima do Peso')
-        } else if (imc < 34.9) {
-            mensagemIMC = 'Obesidade';
+        } else if (imc >= 30.0 && imc <= 34.9) {
+            mensagemIMC = 'Obesidade grau I'
             img.setAttribute('src', genero === 'Homem' ? 'imagens/obesidade_homem.png' : 'imagens/obesidade_mulher.png')
-            alert('Obesidade')
-        } else {
+            alert('Obesidade grau I')
+        } else if (imc >= 35 && imc <= 39.9) {
+            mensagemIMC = 'Obesidade grau II'
+            img.setAttribute('src', genero === 'Homem' ? 'imagens/obesidade_homem.png' : 'imagens/obesidade_mulher.png')
+            alert('Obesidade grau II')        
+        } else (imc >= 40) {
             mensagemIMC = 'Obesidade Morbida';
             img.setAttribute('src', genero === 'Homem' ? 'imagens/obesidademorbida_homem.png' : 'imagens/obesidademorbida_mulher.png')
             alert('Obesidade Morbida')
         }
 
         res.style.textAlign = 'center'
-        res.innerHTML = `${nome} Detectamos ${genero} com ${idade} anos. seu Imc e ${imc} Você Está ${mensagemIMC}`
+        res.innerHTML = `${nome} Detectamos ${genero} com ${idade} anos. seu Imc e ${imc.toFixed(2)} Você Está ${mensagemIMC}`
         res.appendChild(img)
     }
 }
