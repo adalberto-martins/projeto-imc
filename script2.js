f2203f5910f5fd11c30f62441a5dd0d74d1565bb
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-09-02 01:51:24 -0300

    upgrade

--- script2.js
+++ script2.js
@@ -29,31 +29,33 @@ function verificar() {
         if (imc <= 18.5) {
             mensagemIMC = 'Abaixo do Peso'
             img.setAttribute('src', genero === 'Homem' ? 'imagens/magro_homem.png' : 'imagens/magro_mulher.png')
-            alert('Abaixo do Peso')
         } else if (imc >= 18.6 && imc <= 24.9) {
             mensagemIMC = 'Peso ideal (Parabéns!)'
             img.setAttribute('src', genero === 'Homem' ? 'imagens/normal_homem.png' : 'imagens/normal_mulher.png')
-            alert('Peso ideal (Parabéns!)')
         } else if (imc >= 25 && imc <=29.9) {
             mensagemIMC = 'Levemente acima do Peso'
             img.setAttribute('src', genero === 'Homem' ? 'imagens/sobrepeso_masculino.png' : 'imagens/sobrepeso_mulher.png')
-            alert('Levemente acima do Peso')
         } else if (imc >= 30.0 && imc <= 34.9) {
             mensagemIMC = 'Obesidade grau I'
             img.setAttribute('src', genero === 'Homem' ? 'imagens/obesidade_homem.png' : 'imagens/obesidade_mulher.png')
-            alert('Obesidade grau I')
         } else if (imc >= 35 && imc <= 39.9) {
             mensagemIMC = 'Obesidade grau II'
             img.setAttribute('src', genero === 'Homem' ? 'imagens/obesidade_homem.png' : 'imagens/obesidade_mulher.png')
-            alert('Obesidade grau II')        
         } else  {
             mensagemIMC = 'Obesidade Morbida'
             img.setAttribute('src', genero === 'Homem' ? 'imagens/obesidademorbida_homem.png' : 'imagens/obesidademorbida_mulher.png')
-            alert('Obesidade Morbida')
         }
 
+        // Removendo o alerta, pois a informação já será exibida na tela.
+        // window.alert(mensagemIMC); 
+
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${nome}, ${genero} com ${idade} anos. <br/>seu Imc e ${imc.toFixed(2)} Você Está ${mensagemIMC}`
-        res.appendChild(img)
+        res.innerHTML = `Detectamos ${nome}, ${genero} com ${idade} anos.<br/>Seu IMC é ${imc.toFixed(2)}. Você está ${mensagemIMC}.`
+
+        // Novo código para envolver a imagem em uma div
+        var divFoto = document.createElement('div');
+        divFoto.setAttribute('class', 'foto-container');
+        divFoto.appendChild(img);
+        res.appendChild(divFoto);
     }
 }
